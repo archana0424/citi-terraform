78e60288d9e72c0148e746d7f2e7922bbcb14177
archana0424 <sarchana0517@gmail.com>
2026-03-11 14:49:57 +0000

    add

--- root/versions.tf
+++ root/versions.tf
@@ -1,14 +1,3 @@
 terraform {
   required_version = ">= 1.6.0"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.40"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.40"
-    }
-  }
 }
