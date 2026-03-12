790b67b8db06c8e47f4d5657458a7edcb53e4ca2
archana0424 <sarchana0517@gmail.com>
2026-03-12 02:08:59 +0000

    add changes in root main

--- modules/vpn/output.tf
+++ modules/vpn/output.tf
@@ -1,3 +1,4 @@
 output "gateway_ip" {
-  value = google_compute_ha_vpn_gateway.vpn.gateway_ip
+  description = "Public IP of HA VPN gateway interface 0"
+  value       = google_compute_ha_vpn_gateway.gateway.vpn_interfaces[0].ip_address
 }
